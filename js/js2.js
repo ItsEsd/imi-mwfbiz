65add4e6c7eb2902cdb10f79bc4eebc2779d1083
ItsEsd <81977626+ItsEsd@users.noreply.github.com>
2022-04-12 02:43:07 +0530

    Update js2.js

--- js/js2.js
+++ js/js2.js
@@ -123,10 +123,11 @@ document.getElementById("YoutubeOne").innerHTML += '<div class="iteam"><div clas
 var s, j;
 var news = JSON.parse(res[0].imiNews);
 var s = news.newsTitle.length;
-var n = Math.floor((Math.random() * s - 1) + 1);
-//  for (prop in news.newsTitle[n]) {
-document.getElementById("News1").innerHTML = '<div class="container newz"> <div class="iteam-news"> <div class="wrapTODPre card"><a target="_blank" href="' + news.newsLink[n] + '"> <img class="card-img-top cardImg" src="' + news.newsThumb[n] + '"> <div class="card-body bg-light text-dark "> <h4>' + news.newsTitle[n] + '</h4> </div> </a> <div class=" bg-danger text-white" style="text-align:left;"> <p>' + news.conTitle1[n] + '<a target="_blank" class="readmore" href="' + news.conLink1[n] + '">' + news.conSiteName1[n] + '</a> </p> <hr> <p>' + news.conTitle2[n] + '<a target="_blank" class="readmore" href="' + news.conLink2[n] + '">' + news.conSiteName2[n] + '</a></p> </div> </div> </div> </div>';
-//  }
+//var n = Math.floor((Math.random() * s - 1) + 1);
+var n;
+  for (n=0;n<=s-1;n++) {
+document.getElementById("News1").innerHTML += '<div class="swiper-slide"><div class="container newz"> <div class="iteam-news"> <div class="wrapTODPre card"><a target="_blank" href="' + news.newsLink[n] + '"> <img class="card-img-top cardImg" src="' + news.newsThumb[n] + '"> <div class="card-body bg-light text-dark "> <h4>' + news.newsTitle[n] + '</h4> </div> </a> <div class=" bg-danger text-white" style="text-align:left;"> <p>' + news.conTitle1[n] + '<a target="_blank" class="readmore" href="' + news.conLink1[n] + '">' + news.conSiteName1[n] + '</a> </p> <hr> <p>' + news.conTitle2[n] + '<a target="_blank" class="readmore" href="' + news.conLink2[n] + '">' + news.conSiteName2[n] + '</a></p> </div> </div> </div> </div> </div>';
+  }
 var y = Math.floor((Math.random() * 6) + 1);
 var quotes = JSON.parse(res[0].HayQuotes);
 var bio = (res[0].Bio2).split(';');
