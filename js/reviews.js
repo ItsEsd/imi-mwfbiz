d9de2395dcb3f7a6287ade023bfabbed04a8af12
ItsEsd <81977626+ItsEsd@users.noreply.github.com>
2022-04-13 05:28:48 +0530

    Update reviews.js

--- js/reviews.js
+++ js/reviews.js
@@ -1,26 +1,29 @@
-function showreviews(){
+function showreviews(lksee){
     $('#commentInbox').empty();
     $('#rvform').hide();
     $('#sdview').show();
-    document.getElementById("rvform").reset();
     document.getElementById('submitview').disabled=false;
     document.getElementById('ref').innerHTML="Refresh";
     document.getElementById('loader-rv').style.display="block";
     document.getElementById('mviews').style.display="block";
+    if(lksee ==undefined){
+        var lksee = document.getElementById("entry.2036725465").value;
+    }
+var countcmnt = "n";
 var ur1="https://script.google.com/macros/s/";
 var ur2="AKfycbyzJPAp1Ep0pwT74LnzdvbnRj7xxGcfWj1chl5gmqC3cUnQNWk5gP05ltgycDHpr2djpg";
 var url= ur1+ur2+"/exec"+"?action=read";
 jQuery.getJSON(url, function(json) {
-   // console.log(json.records.length);
     json.records.reverse();
-     for (var i = 1; i < json.records.length ; i++) {
-document.getElementById('commentInbox').innerHTML +="<div><p style='font-weight:bold;font-size:16px;color:#161616;'>"+json.records[i].Name+
+     for (var i = 1; i < json.records.length; i++) {
+         if(json.records[i].AdminId == lksee){
+document.getElementById('commentInbox').innerHTML +="<div class='reviewelm'><p class='rvelmpone' style='font-weight:bold;font-size:16px;color:#161616;'>"+json.records[i].Name+
 "<span style='float:right;font-size:12px;font-weight:normal;padding-top:4px;'>"+json.records[i].Timestamp+"</span></p>"+
-"<p style='color:#161616;font-size:14px;'>"+json.records[i].Comment+"</p></div><hr>"
-              
-     }
-     if(json.records.length==1){
-        document.getElementById('commentInbox').innerHTML ="<div><center><h4>No Reviews. Be first to give a Review.</h4></center></div>";
+"<p class='rvelmptwo' style='color:#161616;font-size:14px;'>"+json.records[i].Comment+"</p></div><hr>"
+      countcmnt ="y";}
+    }
+    if(countcmnt =="n"){
+        document.getElementById('commentInbox').innerHTML ="<div class='norevwyt'><center><h4>No Reviews. Be first to give a Review.</h4></center></div>";
      }
      document.getElementById('loader-rv').style.display="none";
 });
