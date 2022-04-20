b3883f1e32c835246536f4bffb5e30f9049437fb
ItsEsd <81977626+ItsEsd@users.noreply.github.com>
2022-04-21 03:01:02 +0530

    Update myimi-script.js

--- js/myimi-script.js
+++ js/myimi-script.js
@@ -225,7 +225,7 @@ else if(res[0].GIFGifts!=''){
 var motostr = res[0].ADMIINMotto;
 var motocon = motostr.split('{biz}');
 document.getElementById('motoincon').style.display = 'block';
-  document.getElementById('motoincon').innerHTML = '<div class="motocondone"><img class="mottoconimg" src="'+motocon[0]+'"><span class="motocontit">'+motocon[1]+'</span></div><hr><div class="motocondtwo">'+motocon[2]+'</div>';
+  document.getElementById('motoincon').innerHTML = '<div class="motocondone"><img class="mottoconimg" src="'+motocon[0]+'"><div class="motocontit">'+motocon[1]+'</div></div><hr><div class="motocondtwo">'+motocon[2]+'</div>';
   document.getElementById("loader_wall").style.display = "none";
   document.getElementById("guru_eye").style.display = "none";
  
@@ -391,7 +391,7 @@ document.getElementById("Bio2").innerHTML ='<div style="max-width:600px;"><p>' +
 var motostr = res[0].ADMIINMotto;
 var motocon = motostr.split('{biz}');
 document.getElementById('motoincon').style.display = 'block';
-document.getElementById('motoincon').innerHTML = '<div class="motocondone"><img class="mottoconimg" src="'+motocon[0]+'"><span class="motocontit">'+motocon[1]+'</span></div><hr><div class="motocondtwo">'+motocon[2]+'</div>';
+document.getElementById('motoincon').innerHTML = '<div class="motocondone"><img class="mottoconimg" src="'+motocon[0]+'"><div class="motocontit">'+motocon[1]+'</div></div><hr><div class="motocondtwo">'+motocon[2]+'</div>';
 document.getElementById("loader_wall").style.display = "none";
 document.getElementById("guru_eye").style.display = "none";
 
