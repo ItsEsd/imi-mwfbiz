3f4d4bbdab55ca4cfdadf2e8d146b50b92cae18f
ItsEsd <81977626+ItsEsd@users.noreply.github.com>
2022-11-30 03:47:29 +0530

    Update reviews.js

--- js/reviews.js
+++ js/reviews.js
@@ -20,12 +20,12 @@ jQuery.getJSON(url, function(json) {
 document.getElementById('commentInbox').innerHTML +="<div class='reviewelm'><p class='rvelmpone' style='font-weight:bold;font-size:16px;color:#161616;'>"+json.records[i].Name+
 "<span style='float:right;font-size:12px;font-weight:normal;padding-top:4px;'>"+json.records[i].Timestamp+"</span></p>"+
 "<p class='rvelmptwo' style='color:#161616;font-size:14px;'>"+json.records[i].Comment+"</p></div>"+
-"<div align='right'><button class='delcombtn' onclick='delrvcomment(this)'>Delete Review <svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' class='bi bi-trash3' viewBox='0 0 16 16'>"+
+"<div align='right'><button class='delcombtn' onclick='delrvcomment(this)'>Delete Comment <svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' class='bi bi-trash3' viewBox='0 0 16 16'>"+
 "<path d='M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z'/> </svg></button><input class='rvcmntid' style='display:none;' value='"+json.records[i].CommentId+"'></div>"+"<hr>"
       countcmnt ="y";}
     }
     if(countcmnt =="n"){
-        document.getElementById('commentInbox').innerHTML ="<div class='norevwyt'><center><h4>No Reviews. Be first to give a Review.</h4></center></div>";
+        document.getElementById('commentInbox').innerHTML ="<div class='norevwyt'><center><h4>No Comments. Be first to give a Comment.</h4></center></div>";
      }
      document.getElementById('loader-rv').style.display="none";
      var user = elGetCookie("_ybizc0"); 
@@ -38,7 +38,7 @@ document.getElementById('commentInbox').innerHTML +="<div class='reviewelm'><p c
 });
 }
 function viewrform(){
-    document.getElementById("ref").innerHTML="See Reviews";
+    document.getElementById("ref").innerHTML="See Comments";
     $("#sdview").hide();
     $("#mviews").hide();$("#rvform").show();
 }
@@ -51,10 +51,10 @@ function delrvcomment(label){
          var cmntid = x[posofinput].value;
          console.log(cmntid);
          var ur1="https://script.google.com/macros/s/";
-         var ur2="AKfycbxPr1k5w2MRyb9RieX_LMyZySi2YE7ED-QdhiNPlgbT61UhY6W01RSPhdf01eZznfWheg";
+         var ur2="AKfycbzxxs9rYu1E8dbAljk38i5XIjj_73bpGGDo-r_tt1xVtXy4cnkvLwoKiAeagYulqOKQjQ";
          var urcmt_ = ur1+ur2 +"/exec"
          var url = urcmt_ + "?callback=ctrlqcmntdel&comntid=" + cmntid + "&action=delcomnt";
-  
+         console.log(url);
          var request = jQuery.ajax({
            crossDomain: true,
            url: url,
