2d95a64e42bf913a8213c41f4bef667c2f42b4a0
soubhikdas <d.soubhik@hotmail.com>
2023-11-14 21:30:17 +0530

    Update myimi-script.js

--- js/myimi-script.js
+++ js/myimi-script.js
@@ -362,7 +362,7 @@ $('#elcondiv').empty(); reldelts();show_wall(window.atob(usd));
 }
 var crnwelm = document.createElement('div');
 crnwelm.id="rfshedtr";
-crnwelm.innerHTML="<a>⭾ ELITE</a>";
+crnwelm.innerHTML="<a>&#11134; ELITE</a>";
 $('body').append(crnwelm);
 document.getElementById('rfshedtr').addEventListener('click',function(){entrnwelp()});
 function entrnwelp(){
